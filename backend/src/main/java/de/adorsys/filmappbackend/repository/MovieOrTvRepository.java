fdf5f4d349b98aa19c54f679691ff6e7f7da5abc
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 16:27:42 +0200

    Cleaned MovieOrTvRepository.java

--- backend/src/main/java/de/adorsys/filmappbackend/repository/MovieOrTvRepository.java
+++ backend/src/main/java/de/adorsys/filmappbackend/repository/MovieOrTvRepository.java
@@ -1,6 +1,5 @@
 package de.adorsys.filmappbackend.repository;
 
-
 import de.adorsys.filmappbackend.domain.MovieOrTvElement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
