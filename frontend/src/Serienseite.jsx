7779fdda060c454422cf9ee0050f9a26dfc50dd3
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 14:48:36 +0200

    Cleaned Serienseite.jsx
    removed old out-commented code, 
    replaced var with let and const, 
    added keys to FilmContainer componets

--- frontend/src/Serienseite.jsx
+++ frontend/src/Serienseite.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import axios from "axios";
 import './FilmContainer.jsx';
-import {FilmContainer} from "./FilmContainer";
+import { FilmContainer } from "./FilmContainer";
 import './styles.scss';
-import {BewertungVorhanden} from "./BewertungVorhanden";
+import { BewertungVorhanden } from "./BewertungVorhanden";
 
 export class Serienseite extends React.Component {
     constructor(props) {
@@ -16,23 +16,19 @@ export class Serienseite extends React.Component {
     componentDidMount = () => {
         axios({
             method: "get",
-            url:
-                'https://api.themoviedb.org/3/tv/popular?api_key=f73409a3f2d4b75bd0d49e840b299fc8&language=en-US&page=1',
+            url: 'https://api.themoviedb.org/3/tv/popular?api_key=f73409a3f2d4b75bd0d49e840b299fc8&language=en-US&page=1',
         }).then(response => {
             let filmInformationen = [];
-            var results = response.data.results;
-            for (var i = 0; i < results.length; i++) {
-                var imagepath =
-                    "https://image.tmdb.org/t/p/w1280" + results[i].poster_path;
+            const results = response.data.results;
+            for (let i = 0; i < results.length; i++) {
+                const imagepath = "https://image.tmdb.org/t/p/w1280" + results[i].poster_path;
+                const ungeteilteBewertung = results[i].vote_average;
+                const bewertung = Math.round(ungeteilteBewertung / 2);
+                const movieOrTv = "tv";
+                const nameOrTitle = results[i].original_name;
+                const erscheinungsJahr = results[i].first_air_date;
+                const id = results[i].id;
 
-
-                var ungeteilteBewertung = results[i].vote_average;
-                var bewertung = Math.round(ungeteilteBewertung / 2);
-                var movieOrTv = "tv";
-                var nameOrTitle = results[i].original_name;
-                var erscheinungsJahr = results[i].first_air_date;
-
-                var id = results[i].id;
                 filmInformationen.push({
                     nameOrTitle,
                     imagepath,
@@ -40,8 +36,7 @@ export class Serienseite extends React.Component {
                     id,
                     movieOrTv,
                     erscheinungsJahr
-                })
-
+                });
             }
             this.setState({
                 filmContainer: filmInformationen
@@ -49,15 +44,13 @@ export class Serienseite extends React.Component {
         });
     }
 
-
     render = () => {
-        //console.log("Serien MovieOrTv: " +)
         return (
             <>
                 <div className="containerGrid" id="fid">
-                    {this.state.filmContainer.map(props => <FilmContainer {...props} />)}
+                    {this.state.filmContainer.map(props => <FilmContainer key={props.id} {...props} />)}
                 </div>
             </>
-        )
+        );
     };
 }
