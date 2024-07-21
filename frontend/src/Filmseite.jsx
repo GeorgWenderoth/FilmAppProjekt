d0881a125365687161d09aa01ee220845156a65f
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 16:16:29 +0200

    Cleaned Filmseite.jsx
    removed old out-commented code

--- frontend/src/Filmseite.jsx
+++ frontend/src/Filmseite.jsx
@@ -19,17 +19,16 @@ export class Filmseite extends React.Component {
                 'https://api.themoviedb.org/3/movie/popular?api_key=f73409a3f2d4b75bd0d49e840b299fc8&language=en-US&page=1',
         }).then(response => {
             let filmInformationen = [];
-            var results = response.data.results;
+            const results = response.data.results;
             for (var i = 0; i < results.length; i++) {
-                var imagepath =
-                    "https://image.tmdb.org/t/p/w1280" + results[i].poster_path;
-
-                var ungeteilteBewertung = results[i].vote_average;
-                var bewertung = Math.round(ungeteilteBewertung / 2);
-                var movieOrTv = "movie";
-                var nameOrTitle = results[i].original_title;
-                var erscheinungsJahr = results[i].release_date;
-                var id = results[i].id;
+                var imagepath = "https://image.tmdb.org/t/p/w1280" + results[i].poster_path;
+                const ungeteilteBewertung = results[i].vote_average;
+                const bewertung = Math.round(ungeteilteBewertung / 2);
+                const movieOrTv = "movie";
+                const nameOrTitle = results[i].original_title;
+                const erscheinungsJahr = results[i].release_date;
+                const id = results[i].id;
+                
                 filmInformationen.push({
                     nameOrTitle,
                     imagepath,
@@ -38,7 +37,6 @@ export class Filmseite extends React.Component {
                     movieOrTv,
                     erscheinungsJahr
                 })
-
             }
             this.setState({
                 filmContainer: filmInformationen
