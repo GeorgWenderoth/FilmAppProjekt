944d09b3df114e11f66c3d42177a3db3a0382e25
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 16:09:56 +0200

    Cleaned test.jsx

--- frontend/src/test.jsx
+++ frontend/src/test.jsx
@@ -1,15 +1,12 @@
 import {Redirect, Route} from "react-router-dom";
 import axios from "axios";
 
-
 export function test(res){
-
-    const jwt = document.cookie;//Cookies.get('__session');
+    const jwt = document.cookie;
     try{
         const res =  axios.get("http://localhost:8080/api/test/user", {headers:{ Authorization: 'Bearer ' + jwt}})
        return
     }catch (err) {
         console.log(err);
     }
-
 }
