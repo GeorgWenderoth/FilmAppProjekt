46d4ab7f94add30e47922bb2bbc3bafe0481ed64
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 16:28:40 +0200

    Cleaned UserRepository.java

--- backend/src/main/java/de/adorsys/filmappbackend/repository/UserRepository.java
+++ backend/src/main/java/de/adorsys/filmappbackend/repository/UserRepository.java
@@ -1,10 +1,8 @@
 package de.adorsys.filmappbackend.repository;
 
-
 import de.adorsys.filmappbackend.domain.UserElement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
