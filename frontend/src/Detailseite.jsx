f70b3b46a4126d9542893d3edfe3d737a859c45e
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 15:19:07 +0200

    Cleaned Detailseite.jsx
    removed old out-commented code, 
    replaced var with let and const,

--- frontend/src/Detailseite.jsx
+++ frontend/src/Detailseite.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from "axios";
 import "./DetailFilmContainer";
-import {DetailFilmContainer} from "./DetailFilmContainer";
+import { DetailFilmContainer } from "./DetailFilmContainer";
 
 export class Detailseite extends React.Component {
     constructor(props) {
@@ -10,12 +10,12 @@ export class Detailseite extends React.Component {
     }
 
     componentDidMount = () => {
-        var searchParams = new URLSearchParams(window.location.search);
+        const searchParams = new URLSearchParams(window.location.search);
         searchParams.has("id");
-        var id = searchParams.get("id");
-        var searchType = new URLSearchParams(window.location.search);
+        const id = searchParams.get("id");
+        const searchType = new URLSearchParams(window.location.search);
         searchType.has("movieOrTv");
-        var movieOrTv = searchType.get("movieOrTv");
+        const movieOrTv = searchType.get("movieOrTv");
 
         axios({
             method: "get",
@@ -25,36 +25,36 @@ export class Detailseite extends React.Component {
                 id +
                 "?api_key=f73409a3f2d4b75bd0d49e840b299fc8&language=en-US",
         }).then(response => {
-                var imagepath = "https://image.tmdb.org/t/p/w1280" + response.data.poster_path;
-                var name = response.data.original_name;
-                var titel = response.data.original_title;
-                var bewertung = response.data.vote_average;
-                var overview = response.data.overview;
-                var runtime = response.data.runtime;
-                var nameOrTitle;
-                switch (movieOrTv) {
-                    case "tv":
-                        nameOrTitle = name;
-                        break;
-                    case "movie":
-                        nameOrTitle = titel;
-                        break;
-                }
-                this.setState({imagepath, nameOrTitle, bewertung, overview, runtime, movieOrTv, id});
+            const imagepath = "https://image.tmdb.org/t/p/w1280" + response.data.poster_path;
+            const name = response.data.original_name;
+            const titel = response.data.original_title;
+            const bewertung = response.data.vote_average;
+            const overview = response.data.overview;
+            const runtime = response.data.runtime;
+            let nameOrTitle;
+
+            switch (movieOrTv) {
+                case "tv":
+                    nameOrTitle = name;
+                    break;
+                case "movie":
+                    nameOrTitle = titel;
+                    break;
             }
-        )
+
+            this.setState({ imagepath, nameOrTitle, bewertung, overview, runtime, movieOrTv, id });
+        });
     }
 
-    render = () => { // Ist das mit den Serch params nicht unsauber?
+    render = () => { 
         return (
-
             <>
                 <div className="detailsContainer" id="fid">
-                    <DetailFilmContainer {...this.state} movieOrTv={new URLSearchParams
-                    (window.location.search)
-                        .get("movieOrTv")} id={new URLSearchParams
-                    (window.location.search)
-                        .get("id")}/>
+                    <DetailFilmContainer 
+                        {...this.state} 
+                        movieOrTv={new URLSearchParams(window.location.search).get("movieOrTv")} 
+                        id={new URLSearchParams(window.location.search).get("id")}
+                    />
                 </div>
             </>
         );
