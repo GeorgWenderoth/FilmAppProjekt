6e35386b6adee8d67b48b3d63f306b8529cbb638
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 17:08:03 +0200

    Cleaned JwtUtils.java

--- backend/src/main/java/de/adorsys/filmappbackend/Security/jwt/JwtUtils.java
+++ backend/src/main/java/de/adorsys/filmappbackend/Security/jwt/JwtUtils.java
@@ -14,7 +14,7 @@ import io.jsonwebtoken.*;
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("${adorsys.app.jwtSecret}") //Was mus da bei mir rein
+    @Value("${adorsys.app.jwtSecret}") 
     private String jwtSecret;
 
     @Value("${adorsys.app.jwtExpirationMs}")
