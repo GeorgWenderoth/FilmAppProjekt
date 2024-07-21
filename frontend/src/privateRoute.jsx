2474a1613d4c4b11895a9bfd97e49ad084279e22
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 13:33:39 +0200

    Cleaned privateRoute.jsx
    Removed the commented-out old code

--- frontend/src/privateRoute.jsx
+++ frontend/src/privateRoute.jsx
@@ -5,47 +5,23 @@ import axios from "axios";
 import {useEffect, useState} from "react";
 
 
-
-
 export function PrivateRoute ({component: Component, ...rest}) {
     const jwt = document.cookie;
-
-
-        const [data, updateData] = useState(false);
+    const [data, updateData] = useState(false);
     const [isLoading, setLoading] = useState(true);
-        useEffect(() => {
-            const getData = async () => {
-                const resp = await axios.get('http://localhost:8080/api/test/user',{
-                    headers: {Authorization: `Bearer ${jwt}`}
-                });
-                const json = await resp.data;
-                console.log("PR: " , json);
-                updateData(json);
-                setLoading(false);
-            }
-            getData();
-        }, []);
+    
+    useEffect(() => {
+        const getData = async () => {
+            const resp = await axios.get('http://localhost:8080/api/test/user',{
+                headers: {Authorization: `Bearer ${jwt}`}
+            });
+            const json = await resp.data;
+            updateData(json);
+            setLoading(false);
+        }
+        getData();
+    }, []);
 
-       //
-
-/*
-   async function back() {
-     try{
-
-         const a = await axios.get("http://localhost:8080/api/test/user",
-             {
-                 headers: {Authorization: `Bearer ${jwt}`}
-
-             });
-         return a;
-     }catch (error){
-         console.log("Error: " , error);
-       }
-   }
-
-    let b = back().then(function (resolve){return resolve.data}).
-    catch(function (error){console.log(error)})
-    */
     if(isLoading) {
         return  <div>Loading ...</div>
     }
@@ -58,6 +34,4 @@ export function PrivateRoute ({component: Component, ...rest}) {
                     : <Redirect to={{pathname: '/login', state: {from: props.location}}}/>}
             />
         )
-
-
 }
