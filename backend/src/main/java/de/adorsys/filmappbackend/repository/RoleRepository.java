c3b628674f14ab8abed2458a0ab2a814798a28d5
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 16:28:18 +0200

    Cleaned RoleRepository.java

--- backend/src/main/java/de/adorsys/filmappbackend/repository/RoleRepository.java
+++ backend/src/main/java/de/adorsys/filmappbackend/repository/RoleRepository.java
@@ -1,15 +1,12 @@
 package de.adorsys.filmappbackend.repository;
 
-
 import de.adorsys.filmappbackend.domain.ERole;
 import de.adorsys.filmappbackend.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(ERole name);
-
 }
