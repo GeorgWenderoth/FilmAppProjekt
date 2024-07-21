a82121296f18e72196cf9841051d12b5f3aff376
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 15:35:47 +0200

    Cleaned BewertungVorhanden.jsx

--- frontend/src/BewertungVorhanden.jsx
+++ frontend/src/BewertungVorhanden.jsx
@@ -6,10 +6,8 @@ class BewertungVorhanden extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
             bewertung: this.props.bewertung,
             vorhandenOderNicht: false
-
         }
     }
 
@@ -17,10 +15,9 @@ class BewertungVorhanden extends React.Component {
         axios({
             method: "get",
             url: 'http://127.0.0.1:8080/element' + "?title=" + this.props.nameOrTitle + "&year=" + this.props.erscheinungsJahr
-
+            
         }).then(response => {
                 if (response.data !== "Das gesuchte Element wurde nicht gefunden") {
-
                     this.setState({bewertung: response.data.bewertungsWert})
                     this.setState({vorhandenOdernicht: true})
                 }
@@ -29,8 +26,6 @@ class BewertungVorhanden extends React.Component {
     }
 
     render = () => {
-        console.log("State: " + this.state.bewertung);
-        console.log(this.props.nameOrTitle + " VORHANDEN Render BV: " + this.state.vorhandenOderNicht)
         return (
             <>
                 {<FilmContainer nameOrTitle={this.props.nameOrTitle}
@@ -40,7 +35,6 @@ class BewertungVorhanden extends React.Component {
                                 movieOrTv={this.props.movieOrTv}
                                 erscheinungsJahr={this.props.erscheinungsJahr}
                                 vorhandenOderNicht={this.state.vorhandenOderNicht}/>}
-
             </>
         )
     };
