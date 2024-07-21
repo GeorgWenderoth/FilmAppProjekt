c8f3f218dead3b210c6cb7324c1074f5e4f7735c
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 16:27:08 +0200

    Cleaned ExceptionHandlerAdvice.java
    corrected comments

--- backend/src/main/java/de/adorsys/filmappbackend/exception/ExceptionHandlerAdvice.java
+++ backend/src/main/java/de/adorsys/filmappbackend/exception/ExceptionHandlerAdvice.java
@@ -1,7 +1,6 @@
 package de.adorsys.filmappbackend.exception;
 
 import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,10 +10,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ExceptionHandlerAdvice {
     @ResponseBody                 // The @ResponseBody annotation tells a controller that the object returned is automatically // serialized into JSON and passed back into the HttpResponse object.
     @ResponseStatus(HttpStatus.BAD_REQUEST) // HttpStatus der zurückgegeben werden soll
-    @ExceptionHandler(UngueltigeBewertung.class) //Informationen zur Fehlerklasse die Gehandelt werden sollen
+    @ExceptionHandler(UngueltigeBewertung.class) //Informationen zur Fehlerklasse die gehandelt werden sollen
     public String ungueltigeBewertungExceptionHandler(UngueltigeBewertung ex){ //methode
         return ex.getMessage();
     }
+    
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ElementNichtVorhanden.class)
